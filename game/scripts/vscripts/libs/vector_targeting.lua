f103a75cc7e1bb5ca7ba72485575e4104dc78beb
Yahnich <19353059+Yahnich@users.noreply.github.com>
2019-12-15 00:55:10 +0100

    fixed not casting outside of cast range

--- game/scripts/vscripts/libs/vector_targeting.lua
+++ game/scripts/vscripts/libs/vector_targeting.lua
@@ -29,14 +29,21 @@ function VectorTarget:StartVectorCast( event )
 	direction = Vector(direction.x, direction.y, 0)
 
 	if ability then
-		unit.inVectorCast = nil
+		unit.isVectorCasting = true
 		self.vectorTargetPosition = position
 		self.vectorTargetPosition2 = position2
 		self.vectorTargetDirection = direction
-		unit:CastAbilityOnPosition(position, ability, event.playerID)
+		ExecuteOrderFromTable({
+			UnitIndex = event.unit,
+			OrderType = DOTA_UNIT_ORDER_CAST_POSITION,
+			Position = position,
+			AbilityIndex = event.abilityIndex
+		})
 		local function OverrideSpellStart(self, position, direction)
 			self:OnVectorCastStart(position, direction)
 		end
+		unit.inVectorCast = nil
+		unit.isVectorCasting = false
 		ability.OnSpellStart = function(self) return OverrideSpellStart(self, position, direction) end
 	end
 end
@@ -61,7 +68,7 @@ CANCEL_EVENT = {[DOTA_UNIT_ORDER_MOVE_TO_POSITION] = true,
 function VectorTarget:OrderFilter(event)
 	if not event.units["0"] then return true end
 	local unit = EntIndexToHScript(event.units["0"])
-	if event.entindex_ability > 0 then
+	if event.entindex_ability > 0 and not unit.isVectorCasting then
 		local ability = EntIndexToHScript(event.entindex_ability)
 		if not ability then return true end
 		local playerID = unit:GetPlayerID()
@@ -78,7 +85,7 @@ function VectorTarget:OrderFilter(event)
 			CustomGameEventManager:Send_ServerToPlayer(player, "vector_target_cast_stop", {cast = unit.inVectorCast == event.entindex_ability})
 			unit.inVectorCast = nil
 			-- filter out 'regular' cast attempt
-			return unit.inVectorCast ~= event.entindex_ability
+			return false
 		end
 	elseif unit.inVectorCast and CANCEL_EVENT[event.order_type] then
 		local playerID = unit:GetPlayerID()
